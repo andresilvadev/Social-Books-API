7190f32cd43212d3ca5d3810984e2e135a2f1265
Andre Luiz Silva <andreluiz1013@hotmail.com>
2016-09-24 00:03:12 -0300

    Search book by id, method search()

--- src/main/java/com/algaworks/socialbooks/resources/BooksResources.java
+++ src/main/java/com/algaworks/socialbooks/resources/BooksResources.java
@@ -3,6 +3,7 @@ package com.algaworks.socialbooks.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,5 +28,9 @@ public class BooksResources {
 	public void save(@RequestBody Book book){
 		booksRepository.save(book);
 	}
-
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public Book search(@PathVariable("id") Long id){
+		return booksRepository.findOne(id);
+	}
 }
