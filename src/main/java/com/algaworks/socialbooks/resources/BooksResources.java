6d015c735bfab60975a20d60acd027a8bdaaa81e
Andre Luiz Silva <andreluiz1013@hotmail.com>
2016-09-23 23:45:26 -0300

    Creating Method save()

--- src/main/java/com/algaworks/socialbooks/resources/BooksResources.java
+++ src/main/java/com/algaworks/socialbooks/resources/BooksResources.java
@@ -3,6 +3,7 @@ package com.algaworks.socialbooks.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,16 +12,20 @@ import com.algaworks.socialbooks.domain.Book;
 import com.algaworks.socialbooks.repository.BooksRepository;
 
 @RestController
+@RequestMapping("/books")
 public class BooksResources {
 	
 	@Autowired
 	private BooksRepository booksRepository;
 	
-	@RequestMapping(value= "/books", method = RequestMethod.GET)
-	public List<Book> list(){
-		
-		return booksRepository.findAll();
-		
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Book> list(){		
+		return booksRepository.findAll();		
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public void save(@RequestBody Book book){
+		booksRepository.save(book);
 	}
 
 }
